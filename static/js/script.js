8e7105dac95a41d831080f9346d7b1913154a203
HappyPantss <sergioeijben@hotmail.com>
2020-06-29 18:02:40 +0200

    Last changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,24 @@
-var today = "29-6-2020";
+// Show todays date in index.html
+const dateMessage = document.querySelector("#todaysDate");
+const checkToday = new Date(); // Check todays date
 
-var motd = "Welcome! Today it is: ";
+// When the window/ site loads, the empty text will change to todays date
+window.addEventListener('load', (event) => {
+    dateMessage.textContent = checkToday; // textContent changes the content to the checkToday value (date)
+})
 
-console.log(this.motd + today);
-
+// Check if password and confirmed password are the same
 const password = document.querySelector("#password");
 const cPassword = document.querySelector("#confirmPassword");
-const cPasswordLabel = document.querySelector("#cPasswordLabel");
+const registerForm = document.querySelector("#registerForm");
 
-function validatePassword(){
-  if (password.value == cPassword.value) {
-    cPassword.style.borderColor = 'green';
-    confirmPassword.setCustomValidity('');
-  } else {
+// When the user has changed the content and field, this code is checking if the passwords are matching
+registerForm.addEventListener('change', (event) => {
+  if (password.value === cPassword.value) { // When passwords match
+    cPassword.style.borderColor = 'green'; 
+    confirmPassword.setCustomValidity(""); 
+  } else if(password.value !== cPassword.value || cPassword.value !== password.value) { // When passwords don't match
     cPassword.style.borderColor = '';
-    confirmPassword.setCustomValidity("Passwords don't match");
+    confirmPassword.setCustomValidity("Passwords don't match!"); // Set custom warning when passwords don't match
   }
-}
-
-password.onchange = validatePassword;
-cPassword.onkeyup = validatePassword;
-
-// function validatePassword(){
-//   if (cPassword.value > 0) {
-//     cPassword.style.backgroundColor = 'green';
-//   } else {
-//     cPassword.style.backgroundColor = '';
-//   }
-// }
+});
